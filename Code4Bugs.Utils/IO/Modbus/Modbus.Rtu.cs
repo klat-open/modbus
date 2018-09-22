37c74b000738033b06d9af9c2844a098e89e3022
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2018-09-22 22:04:31 +0700

    - adjust modbus class structure

---
+++ Code4Bugs.Utils/IO/Modbus/Modbus.Rtu.cs
@@ -0,0 +1,78 @@
+﻿using Code4Bugs.Utils.Types;
+
+namespace Code4Bugs.Utils.IO.Modbus
+{
+    public static partial class Modbus
+    {
+        public static byte[] RequestFunc3(this ICommStream stream, int slaveAddress, int dataAddress, int registerCount)
+        {
+            var message = BuildFunc3And4Message(slaveAddress, 3, dataAddress, registerCount);
+            lock (stream)
+            {
+                stream.Write(message, 0, message.Length);
+
+                var responseLength = 5 + 2 * registerCount;
+                var response = ReceiveMessage(stream, responseLength);
+
+                VerifyEmptyResponse(response);
+                VerifyChecksum(response);
+                VerifyGeneralStructure(response, slaveAddress, 3);
+
+                return response;
+            }
+        }
+
+        public static byte[] RequestFunc4(this ICommStream stream, int slaveAddress, int dataAddress, int registerCount)
+        {
+            var message = BuildFunc3And4Message(slaveAddress, 4, dataAddress, registerCount);
+            lock (stream)
+            {
+                stream.Write(message, 0, message.Length);
+
+                var responseLength = 5 + 2 * registerCount;
+                var response = ReceiveMessage(stream, responseLength);
+
+                VerifyEmptyResponse(response);
+                VerifyChecksum(response);
+                VerifyGeneralStructure(response, slaveAddress, 4);
+
+                return response;
+            }
+        }
+
+        public static byte[] RequestFunc6(this ICommStream stream, int slaveAddress, int dataAddress, int writeValue)
+        {
+            var message = BuildFunc6Message(slaveAddress, dataAddress, writeValue);
+            lock (stream)
+            {
+                stream.Write(message, 0, message.Length);
+
+                var response = ReceiveMessage(stream, 8);
+
+                VerifyEmptyResponse(response);
+                // Response of the func6 is always an echo.
+                if (!response.AreEqual(message))
+                    throw new DataCorruptedException("Response of function 6 should be an echo.");
+
+                return response;
+            }
+        }
+
+        public static byte[] RequestFunc16(this ICommStream stream, int slaveAddress, int dataAddress, byte[] writeValue)
+        {
+            var message = BuildFunc16Message(slaveAddress, dataAddress, writeValue);
+            lock (stream)
+            {
+                stream.Write(message, 0, message.Length);
+
+                var response = ReceiveMessage(stream, 8);
+
+                VerifyEmptyResponse(response);
+                VerifyChecksum(response);
+                VerifyGeneralStructure(response, slaveAddress, 16);
+
+                return response;
+            }
+        }
+    }
+}
--- Code4Bugs.Utils/IO/Modbus/Modbus.cs
+++ Code4Bugs.Utils/IO/Modbus/Modbus.cs
@@ -1,85 +1,11 @@
-﻿using Code4Bugs.Utils.Types;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading;
 
 namespace Code4Bugs.Utils.IO.Modbus
 {
-    public static class ModbusRtu
+    public static partial class Modbus
     {
-        public static byte[] RequestFunc3(this ICommStream stream, int slaveAddress, int dataAddress, int registerCount)
-        {
-            var message = BuildFunc3And4Message(slaveAddress, 3, dataAddress, registerCount);
-            lock (stream)
-            {
-                stream.Write(message, 0, message.Length);
-
-                var responseLength = 5 + 2 * registerCount;
-                var response = ReceiveMessage(stream, responseLength);
-
-                VerifyEmptyResponse(response);
-                VerifyChecksum(response);
-                VerifyGeneralStructure(response, slaveAddress, 3);
-
-                return response;
-            }
-        }
-
-        public static byte[] RequestFunc4(this ICommStream stream, int slaveAddress, int dataAddress, int registerCount)
-        {
-            var message = BuildFunc3And4Message(slaveAddress, 4, dataAddress, registerCount);
-            lock (stream)
-            {
-                stream.Write(message, 0, message.Length);
-
-                var responseLength = 5 + 2 * registerCount;
-                var response = ReceiveMessage(stream, responseLength);
-
-                VerifyEmptyResponse(response);
-                VerifyChecksum(response);
-                VerifyGeneralStructure(response, slaveAddress, 4);
-
-                return response;
-            }
-        }
-
-        public static byte[] RequestFunc6(this ICommStream stream, int slaveAddress, int dataAddress, int writeValue)
-        {
-            var message = BuildFunc6Message(slaveAddress, dataAddress, writeValue);
-            lock (stream)
-            {
-                stream.Write(message, 0, message.Length);
-
-                var response = ReceiveMessage(stream, 8);
-
-                VerifyEmptyResponse(response);
-                // Response of the func6 is always an echo.
-                if (!response.AreEqual(message))
-                    throw new DataCorruptedException("Response of function 6 should be an echo.");
-
-                return response;
-            }
-        }
-
-        public static byte[] RequestFunc16(this ICommStream stream, int slaveAddress, int dataAddress, byte[] writeValue)
-        {
-            var message = BuildFunc16Message(slaveAddress, dataAddress, writeValue);
-            lock (stream)
-            {
-                stream.Write(message, 0, message.Length);
-
-                var response = ReceiveMessage(stream, 8);
-
-                VerifyEmptyResponse(response);
-                VerifyChecksum(response);
-                VerifyGeneralStructure(response, slaveAddress, 16);
-
-                return response;
-            }
-        }
-
-        #region Utilities
-
         private static byte[] BuildFunc16Message(int slaveAddress, int dataAddress, byte[] writeValue)
         {
             //Function 16 request is always 9+ bytes
@@ -157,7 +83,7 @@ namespace Code4Bugs.Utils.IO.Modbus
                 throw new DataCorruptedException($"Wrong response function code. Expected {funcCode}, actual {response[1]}");
         }
 
-        internal static byte[] ReceiveMessage(ICommStream stream, int length)
+        private static byte[] ReceiveMessage(ICommStream stream, int length)
         {
             var timeout = stream.ReadTimeout;
 
@@ -197,7 +123,5 @@ namespace Code4Bugs.Utils.IO.Modbus
             }
             return stream.Available > 0;
         }
-
-        #endregion Utilities
     }
 }
