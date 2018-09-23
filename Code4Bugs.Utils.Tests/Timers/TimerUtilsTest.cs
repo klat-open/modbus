c381ecf0880f68beb4a63bde1ae2ff8042267c29
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2018-09-23 08:22:59 +0700

    + timer utilities

---
+++ Code4Bugs.Utils.Tests/Timers/TimerUtilsTest.cs
@@ -0,0 +1,58 @@
+﻿using Code4Bugs.Utils.Timers;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Code4Bugs.Utils.Tests.Timers
+{
+    [TestFixture]
+    internal class TimerUtilsTest
+    {
+        [Test]
+        public void ExecuteAfter_should_delay_an_action_after_a_period_of_time()
+        {
+            var stopwatch = new Stopwatch();
+            var ellapsedMillis = 0L;
+            Action action = () =>
+            {
+                ellapsedMillis = stopwatch.ElapsedMilliseconds;
+            };
+            action.ExecuteAfter(1000);
+            stopwatch.Start();
+            Thread.Sleep(1100);
+            Assert.GreaterOrEqual(ellapsedMillis, 1000);
+        }
+
+        [Test]
+        public void Loop_should_stop_when_callback_return_false()
+        {
+            var maxLoop = 10;
+            var loopCount = 0;
+            Func<bool> callback = () =>
+            {
+                loopCount++;
+                return loopCount < maxLoop;
+            };
+            callback.Loop(10);
+            Thread.Sleep(maxLoop * 10 + 200);
+            Assert.AreEqual(maxLoop, loopCount);
+        }
+
+        [Test]
+        public void LoopFixed_should_stop_when_callback_return_false()
+        {
+            var maxLoop = 10;
+            var loopCount = 0;
+            Func<bool> callback = () =>
+            {
+                loopCount++;
+                Thread.Sleep(5);
+                return loopCount < maxLoop;
+            };
+            callback.LoopFixed(10);
+            Thread.Sleep(maxLoop * 10 + 200);
+            Assert.AreEqual(maxLoop, loopCount);
+        }
+    }
+}
---
+++ Code4Bugs.Utils/Timers/TimerUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Code4Bugs.Utils.Timers
+{
+    public static class TimerUtils
+    {
+        public static async void ExecuteAfter(this Action action, int delayMillis)
+        {
+            await Task.Delay(delayMillis);
+            action();
+        }
+
+        public static async void ExecuteAfter<T>(this Action<T> action, int delayMillis, T state)
+        {
+            await Task.Delay(delayMillis);
+            action(state);
+        }
+
+        public static void Loop(this Func<bool> callback, int intervalMillis)
+        {
+            Timer timer = null;
+            timer = new Timer(state =>
+            {
+                if (!callback())
+                {
+                    timer?.Dispose();
+                    timer = null;
+                }
+            }, null, 0, intervalMillis);
+            GC.KeepAlive(timer);
+        }
+
+        public static void LoopFixed(this Func<bool> callback, int intervalMillis)
+        {
+            Timer timer = null;
+            var stopwatch = new Stopwatch();
+
+            timer = new Timer(state =>
+            {
+                stopwatch?.Restart();
+
+                if (!callback())
+                {
+                    timer?.Dispose();
+                    timer = null;
+                    stopwatch = null;
+                    return;
+                }
+
+                var elapsedMillis = stopwatch != null ? stopwatch.ElapsedMilliseconds : 0;
+                timer?.Change(Math.Max(0, intervalMillis - elapsedMillis), Timeout.Infinite);
+            }, null, 0, Timeout.Infinite);
+
+            GC.KeepAlive(timer);
+            GC.KeepAlive(stopwatch);
+        }
+    }
+}
