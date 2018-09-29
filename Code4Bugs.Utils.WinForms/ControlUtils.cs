5e871a8ba33e8a5026067f9725d1525bd044da66
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2018-09-29 22:33:02 +0700

    + go through all children of a control

---
+++ Code4Bugs.Utils.WinForms/ControlUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Code4Bugs.Utils.WinForms
+{
+    public static class ControlUtils
+    {
+        public static void Iterate(this Control control, Action<Control> actionOnChild)
+        {
+            actionOnChild(control);
+            foreach (Control child in control.Controls)
+            {
+                Iterate(child, actionOnChild);
+            }
+        }
+
+        public static void Iterate(this Control control, Func<Control, bool> actionOnChild)
+        {
+            if (!actionOnChild(control)) return;
+            foreach (Control child in control.Controls)
+            {
+                Iterate(child, actionOnChild);
+            }
+        }
+    }
+}
