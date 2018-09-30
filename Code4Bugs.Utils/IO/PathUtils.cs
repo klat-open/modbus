1422eb5924a27de3a19b9b9281213d9408b8ac1f
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2018-09-30 18:10:35 +0700

    + path utils

---
+++ Code4Bugs.Utils/IO/PathUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Code4Bugs.Utils.IO
+{
+    public static class PathUtils
+    {
+        public static bool IsValidPath(string path)
+        {
+            try
+            {
+                Path.GetFullPath(path);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
