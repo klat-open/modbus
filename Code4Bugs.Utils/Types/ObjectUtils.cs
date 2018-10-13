34b241407adfd4fcafca580e4d5152f27f405c51
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2018-10-14 02:22:41 +0700

    + convert an object to json string

---
+++ Code4Bugs.Utils/Types/ObjectUtils.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Code4Bugs.Utils.Types
+{
+    public static class ObjectUtils
+    {
+        public static string ToJson(this object obj, bool pretty = false)
+        {
+            if (obj == null)
+                return "NULL";
+
+            return JsonConvert.SerializeObject(obj, pretty ? Formatting.Indented : Formatting.None);
+        }
+    }
+}
